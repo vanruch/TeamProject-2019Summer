b11eaa941c14109f831c957bc1fc3cbe829dd7ae
ivan <ivanruch@gmail.com>
2019-05-31 10:01:31 +0200

    Fixed adding annotations

--- src/Components/Prompt.js
+++ src/Components/Prompt.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import SelectType from './SelectType';
-import ReactTags from "react-tag-autocomplete";
+import ReactTags from 'react-tag-autocomplete';
+import {availableTypes} from '../common';
 
 export default class Prompt extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      type: this.props.type.map(typeSlug => SelectType().find(availableType => availableType.value === typeSlug)),
+      type: this.props.type.map(typeSlug => availableTypes.find(availableType => availableType.value === typeSlug)),
       text: this.props.text
     };
     this.props.onChange(this.state.type.map(type => type.value), this.state.text);
@@ -25,14 +25,14 @@ export default class Prompt extends Component {
   }
 
   onDelete(i) {
-    const types = this.state.type.slice(0)
-    types.splice(i, 1)
-    this.setState({type: types})
+    const types = this.state.type.slice(0);
+    types.splice(i, 1);
+    this.setState({type: types});
   }
 
   onAddition(type) {
-    const types = [].concat(this.state.type, type)
-    this.setState({type: types})
+    const types = [].concat(this.state.type, type);
+    this.setState({type: types});
   }
 
   onValidate(tag) {
@@ -43,15 +43,15 @@ export default class Prompt extends Component {
     return <div>
       Type:
       <ReactTags
-          minQueryLength={1}
-          placeholderText={"Add new type"}
-          tags={this.state.type}
-          suggestions={SelectType()}
-          onValidate={this.onValidate.bind(this)}
-          onDelete={this.onDelete.bind(this)}
-          onAddition={this.onAddition.bind(this)}
+        minQueryLength={1}
+        placeholderText={'Add new type'}
+        tags={this.state.type}
+        suggestions={availableTypes}
+        onValidate={this.onValidate.bind(this)}
+        onDelete={this.onDelete.bind(this)}
+        onAddition={this.onAddition.bind(this)}
       />
-      {this.state.type == "chata_reference" && <div>Text:
+      {this.state.type && this.state.type.includes("chata_reference") && <div>Text:
       <input type="text" className="mm-popup__input" value={this.state.text} onChange={this.onTextChange.bind(this)}/></div>}
     </div>;
   }
